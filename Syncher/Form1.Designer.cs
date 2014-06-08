748e80dd332969f3181bc488856397381406c4ff
sampath <sampath.skt@gmail.com>
2014-06-08 18:47:26 +0530

    can do now file copying

--- Syncher/Form1.Designer.cs
+++ Syncher/Form1.Designer.cs
@@ -31,8 +31,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
@@ -63,40 +61,17 @@
             this.button2.Text = "Sync";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(16, 40);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 3;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(16, 75);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
-            this.checkBox2.TabIndex = 4;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Syncher";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            //this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -108,8 +83,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
 
--- Syncher/Form1.cs
+++ Syncher/Form1.cs
@@ -11,33 +11,82 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        string fName,desFName;
+        bool dirExists = false;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        /*private void Form1_Load(object sender, EventArgs e)
         {
-
-        }
+            String appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            System.IO.DirectoryInfo apD = new System.IO.DirectoryInfo("@" + "\"" + appDirectory + "\"");
+            System.IO.DirectoryInfo[] subDirs = apD.GetDirectories();
+            foreach (System.IO.DirectoryInfo dI in subDirs)
+            {
+                if (dI.Name.Equals("dataFiles"))
+                {
+                    dirExists = true;
+                }
+            }
+            String dataDir = "@" + "\"" + appDirectory + "\\dataFiles" + "\"";
+            if (dirExists == false)
+            {
+                System.IO.Directory.CreateDirectory(dataDir);
+            }
+            
+            Console.WriteLine("app directory "+"@"+"\""+appDirectory+"\"");
+            //System.IO.DirectoryInfo[] subDir = appDirectory.
+        }*/
 
         private void button1_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog folderChooser = new FolderBrowserDialog();
             if (folderChooser.ShowDialog() == DialogResult.OK)
             {
-                String path1 = folderChooser.SelectedPath;
-                Console.WriteLine(path1);
+                String frmpath1 = makePath(folderChooser.SelectedPath);
+                Console.WriteLine(frmpath1);
                 MessageBox.Show("Now add second folder");
                 if (folderChooser.ShowDialog() == DialogResult.OK)
                 {
-                    String path2 = folderChooser.SelectedPath;
-                    Console.WriteLine(path2);
-                    CheckBox foldPair1 = new CheckBox();
-                    foldPair1.Text = path1 + " " + path2;
+                    String topath2 = makePath(folderChooser.SelectedPath);
+                    addFolderPair(frmpath1,topath2);
+                    sync(frmpath1, topath2);
+                    
                 }
 
             }
+        }
+
+        void sync(String frm, String to)
+        {
+            string[] files = System.IO.Directory.GetFiles(frm);
+            System.IO.DirectoryInfo toDir = new System.IO.DirectoryInfo(to);
+            System.IO.DirectoryInfo frmDir = new System.IO.DirectoryInfo(frm);
+            System.IO.DirectoryInfo[] subDirs = frmDir.GetDirectories();
+
+            foreach (string fil in files)
+            {
+                fName = System.IO.Path.GetFileName(fil);
+                desFName = System.IO.Path.Combine(to, fName);
+                System.IO.File.Copy(fil, desFName, true);
+            }
+
+        }
+        String makePath(String pth)
+        {
+            //return pth = "@" + "\"" + pth + "\"";
+            return pth;
+        }
+
+        private void addFolderPair(String f1, String f2)
+        {
+            CheckBox foldPair1 = new CheckBox();
+            foldPair1.AutoSize = true;
+            foldPair1.Text = f1 + " " + f2;
+            foldPair1.Location = new System.Drawing.Point(16, 75);
+            this.Controls.Add(foldPair1);
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
